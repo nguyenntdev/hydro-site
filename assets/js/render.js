39f82aa1968f023d6742ec5d816e020b647a2533
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-09-17 21:22:57 -0600

    Argh

--- assets/js/render.js
+++ assets/js/render.js
@@ -2,10 +2,10 @@ var headerContent = `
 
 
 <div class="topnav nav-gradient" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
+  <a href="#home" class="active">Trang chủ</a>
+  <a href="#news">Blog</a>
+  <a href="https://fb.com/leminhtri.dev">Liên hệ</a>
+  <a href="#about">Tổ chức Kode</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
@@ -14,7 +14,17 @@ var headerContent = `
 `;
 
 var footerContent = `
-    
+  
+<div class="topnav nav-gradient fixed-bottom" id="myTopnav">
+<div class="hydro" style="float:left">
+<span>Hydro Team</span>
+</div>
+
+<div style="float: right">
+<a href="#"><i class="fab fa-facebook"></i></a>
+</>
+</div>
+
 `;
 
 document.getElementById("header").innerHTML = headerContent;
